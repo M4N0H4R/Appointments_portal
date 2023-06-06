75e8da632f3b3098939a1754ec7bcd5e300b3388
WALKER <walker.1100100@gmail.com>
2023-06-06 10:53:58 +0530

    Initial commit

---
+++ MongoDB.py
@@ -0,0 +1,11 @@
+import pymongo
+
+client=pymongo.MongoClient("mongodb+srv://walker:walker617@hod-portal.hpemmk8.mongodb.net/?retryWrites=true&w=majority")
+db=client["database"]
+
+coll=db["faculty_schedule"]
+coll.delete_many({})
+coll=db["student_schedule"]
+coll.delete_many({})
+coll=db["slots_list"]
+coll.delete_many({})
